c264525511fd91c5b8860d4e21c8c3642571f9ba
lubovcha <chalovaljubov@mail.ru>
2022-08-30 23:15:57 +0300

    1

---
+++ LastNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// See Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+System.Console.Write("Введите число: "); 
+ 
+int n = Convert.ToInt32(Console.ReadLine()); 
+ 
+ 
+int lastDigit = n % 10; 
+ 
+System.Console.WriteLine(lastDigit);
