ef4e7ffc7c942f063506c99da0dabc381a1ae9ba
lubovcha <chalovaljubov@mail.ru>
2022-08-30 22:26:50 +0300

    1

---
+++ Days/Days.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите номер дня недели: "); 
+int day = Convert.ToInt32(Console.ReadLine()); 
+ 
+if (day == 1){ 
+    System.Console.WriteLine("Понедельник"); 
+} 
+else if (day == 2){ 
+    System.Console.WriteLine("Вторник"); 
+} 
+else if (day == 3){ 
+    System.Console.WriteLine("Среда"); 
+} 
+else if (day == 4){ 
+    System.Console.WriteLine("Четверг"); 
+} 
+else if (day == 5){ 
+    System.Console.WriteLine("Пятница"); 
+} 
+else if (day == 6){ 
+    System.Console.WriteLine("Суббота"); 
+} 
+else if (day == 7){ 
+    System.Console.WriteLine("Воскресенье"); 
+} 
+else{ 
+    System.Console.WriteLine("Неверный день недели"); 
+}
---
+++ Square/Square.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число a: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+ 
+Console.Write("Введите число b: "); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+ 
+if (a == b * b){ 
+    System.Console.WriteLine("Число a является квадратом числа b"); 
+} 
+else{ 
+    System.Console.WriteLine("Число a не является квадратом числа b"); 
+}
+
+// Программа, которая на вход принимает число и выдает его квадрат (число помноженное на само себя)
